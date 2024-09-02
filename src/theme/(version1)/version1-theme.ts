7fdac179cc5335565bc4504299116ec264b8ec18
kim.jongsik <otw1917@naver.com>
2024-09-02 23:01:08 +0900

    refactor: 코드 포매터 적용

--- src/theme/(version1)/version1-theme.ts
+++ src/theme/(version1)/version1-theme.ts
@@ -1,7 +1,11 @@
 import { BasicButton } from "@/theme/(version1)/buttons/BasicButton";
 import { DisabledButton } from "@/theme/(version1)/buttons/DisabledButton";
 import { EmphasizedButton } from "@/theme/(version1)/buttons/EmphasizedButton";
-import { ButtonType, CardType, ComponentFactoryInterface } from "@/theme/theme.interface";
+import {
+  ButtonType,
+  CardType,
+  ComponentFactoryInterface,
+} from "@/theme/theme.interface";
 import { CardProps } from "../abstracts/AbstractCard";
 import { BasicCard } from "./cards/BasicCard";
 import { ImageCard } from "./cards/ImageCard";
@@ -9,31 +13,29 @@ import { ProfileCard } from "./cards/ProfileCard";
 import { ButtonProps } from "../abstracts/AbstractButton";
 
 export class ComponentV1Factory implements ComponentFactoryInterface {
-    createButton(type: ButtonType, props: ButtonProps): React.ReactElement {
-        switch (type) {
-            case ButtonType.Basic:
-                return new BasicButton({ ...props }).render();
-            case ButtonType.Disabled:
-                return new DisabledButton({ ...props }).render();
-            case ButtonType.Emphasized:
-                return new EmphasizedButton({ ...props }).render();
-            default:
-                throw new Error(`Unsupported button type: ${type}`);
-        }
+  createButton(type: ButtonType, props: ButtonProps): React.ReactElement {
+    switch (type) {
+      case ButtonType.Basic:
+        return new BasicButton({ ...props }).render();
+      case ButtonType.Disabled:
+        return new DisabledButton({ ...props }).render();
+      case ButtonType.Emphasized:
+        return new EmphasizedButton({ ...props }).render();
+      default:
+        throw new Error(`Unsupported button type: ${type}`);
     }
+  }
 
-    createCard(type: string, props: CardProps): React.ReactElement {
-        switch (type) {
-            case CardType.Basic:
-                return new BasicCard({ ...props }).render();
-            case CardType.Image:
-                return new ImageCard({ ...props }).render(); 
-            case CardType.Profile:
-                return new ProfileCard({
-                    ...props
-                }).render();
-            default:
-                throw new Error(`Unsupported card type: ${type}`);
-        }
+  createCard(type: string, props: CardProps): React.ReactElement {
+    switch (type) {
+      case CardType.Basic:
+        return new BasicCard({ ...props }).render();
+      case CardType.Image:
+        return new ImageCard({ ...props }).render();
+      case CardType.Profile:
+        return new ProfileCard({ ...props }).render();
+      default:
+        throw new Error(`Unsupported card type: ${type}`);
     }
+  }
 }
