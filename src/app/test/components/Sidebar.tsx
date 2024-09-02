29f67d5515874efdf9fe16ec1e0463ad08d00ee9
kim.jongsik <otw1917@naver.com>
2024-09-03 01:39:05 +0900

    fix: aside 색상변경

--- src/app/test/components/Sidebar.tsx
+++ src/app/test/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarItemComponent from "./SidebarItemComponent";
 
 export default function Sidebar() {
   return (
-    <aside className="w-64 h-screen bg-gray-800 text-gray-200 p-4 fixed left-0 top-0 overflow-y-auto">
+    <aside className="w-64 h-screen bg-gray-900 text-gray-500 p-4 fixed left-0 top-0 overflow-y-auto">
       {sidebarItems.map((item, index) => (
         <SidebarItemComponent key={index} item={item} />
       ))}
